a0a812e30a1a6d853168124ed39cdcae2f1f29e1
ayub.malik <ayub.malik@gmail.com>
2014-11-04 22:38:10 +0000

    Initial commit

---
+++ src/dkill/handlers/ExitHandler.java
@@ -0,0 +1,21 @@
+package dkill.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * 
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class ExitHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		HandlerUtil.getActiveWorkbenchWindow(event).close();
+		return null;
+	}
+}
